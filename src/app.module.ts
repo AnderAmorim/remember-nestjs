9c02f8f805660d69c8bdb438888ae5ca8801f8c1
Anderson Amorim <andersonamorim939@gmail.com>
2022-04-15 12:32:15 -0300

    connect with mongodb and add nest pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,22 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const urlConnect =
+  'mongodb+srv://Anderson:78789378@cluster0.a4u6y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const parameters = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:123@cluster0.a4u6y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+    JogadoresModule,
+  ],
 })
 export class AppModule {}
